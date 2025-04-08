a388ee68b30a2899a2abd18e1a3083bf773a27c8
ashankar <anirudhshankar99@gmail.com>
2025-04-08 10:53:09 +0200

    orbits_with_mlp

--- Dynamics/dynamics_env/dynamics_env.py
+++ Dynamics/dynamics_env/dynamics_env.py
@@ -6,12 +6,15 @@ except:
     import galaxy_models
 import torch
 from scipy.integrate import solve_ivp
+import torch.nn.functional as F
 
 class Dynamics(gym.Env):
     def __init__(self, hyperparameters):
         self._init_hyperparameters(hyperparameters)
-        self.high = np.array([50.0, 50.0, 50.0, 300.0, 300.0, 300.0, 45.9114146119, 45.9114146119, 45.9114146119])
-        self.low = np.array([-50.0, -50.0, -50.0, -300.0, -300.0, -300.0, 0., 0., 0.])
+        # self.high = np.array([50.0, 50.0, 50.0, 300.0, 300.0, 300.0, 45.9114146119, 45.9114146119, 45.9114146119])
+        self.high = np.array([50.0, 50.0, 50.0, 300.0, 300.0, 300.0])
+        # self.low = np.array([-50.0, -50.0, -50.0, -300.0, -300.0, -300.0, 0., 0., 0.])
+        self.low = np.array([-50.0, -50.0, -50.0, -300.0, -300.0, -300.0])
         self.action_space = gym.spaces.Box(
             low=self.low/10,
             high=self.high/10,
@@ -29,48 +32,52 @@ class Dynamics(gym.Env):
     def step(self, action):
         action = self._process_actions(action)
         self.init_params += action
-        self.orbit = self._calculate_orbit()
-        self.orbit_timesteps = len(self.orbit.y[0])
-        self.orbit_accelerations = self.get_acceleration(self.orbit.y[:,self.orbit_timesteps])
-        return self.init_params, -self.orbit_rewards, False, False, self._get_info() # rewards, dones must be calculated externally
-    
-    def reset_2(self):
-        self.timestep = 0
-        self.phase_coords = np.array(self.orbit.y[:,self.timestep])
-        # cat accelerations
-        return self._normalise_state(self.phase_coords), self._get_info_2()
-    
-    def step_2(self, action):
-        self.timestep += 1
-        action = self._process_actions_2(action)
-        self.phase_coords = np.clip(self.phase_coords + action, self.low[:3], self.high[:3])
-        orbit_phase_coords = np.array(self.orbit.y[:,self.timestep])
-        reward = -self.chi_sq(self.phase_coords, orbit_phase_coords)
-        done = self.timestep == self.orbit_timesteps - 1 or (np.abs(orbit_phase_coords) >= self.high[:3]).any()
-        self.orbit_rewards += reward
-        # cat accelerations
-        return self._normalise_state(self.phase_coords), reward, done, False, self._get_info_2()
+        orbit = self._calculate_orbit()
+        # out_of_box_mask = orbit[:3] > np.expand_dims(self.high[:3], axis=-1)
+        # first_index = np.argmax(out_of_box_mask, axis=1, keepdims=True).min()
+        # orbit = orbit[:,:first_index]
+        orbit_model = self.create_orbit_model()
+        orbit_model_opt = torch.optim.Adam(orbit_model.parameters(), lr=1e-3)
+        prev_states = torch.tensor(orbit[:,:-1], dtype=torch.float32, device=self.device).transpose(dim0=-1, dim1=-2)
+        dest_states = torch.tensor(orbit[:,1:], dtype=torch.float32, device=self.device).transpose(dim0=-1, dim1=-2)
+        for _ in range(self.orbit_model_epochs):
+            x = orbit_model(prev_states)
+            loss = F.mse_loss(x, dest_states)
+            orbit_model_opt.zero_grad()
+            loss.backward()
+            orbit_model_opt.step()
+        orbit_rewards = 0
+        info = {}
+        for delta in self.predicted_orbit_delta_list:
+            delta_orbit = prev_states[:-delta]
+            delta_dest_orbit = dest_states[delta:]
+            for _ in range(delta):
+                with torch.no_grad():
+                    delta_orbit = orbit_model(delta_orbit)
+            orbit_rewards += self.error(delta_dest_orbit, delta_orbit).item() / len(prev_states)
+            info['%d_y'%delta] = delta_dest_orbit
+            info['%d_x'%delta] = delta_orbit
+        return self.init_params, orbit_rewards, False, False, info # rewards, dones must be calculated externally
 
-    def reset(self, seed=None, options=None, init_params=None):
+    def reset(self, init_params=None):
         if init_params == None:
-            self.init_params = self._process_actions((np.random.rand(6,)*2-1) * self.high[:3] / 10)
-            print('[ENV]: Initial parameters not specified, using the following, selected randomly:',self.init_params)
+            self.init_params = self._process_actions((np.random.rand(6,)*2-1) * self.high / 50)
+            print('[ENV]: Initial parameters not specified, using the following, selected randomly:', self.init_params)
         else:
-            assert (np.abs(np.array(init_params)) <= self.high[:3]).all(), "If initial parameters are specified, they must be within the observation space"
+            assert (np.abs(np.array(init_params)) <= self.high).all(), "If initial parameters are specified, they must be within the observation space"
             self.init_params = self._process_actions(np.array(init_params))
-
-        self.phase_coords = self.init_params
-        self.orbit = self._calculate_orbit()
-        self.orbit_timesteps = len(self.orbit.y[0])
-        self.orbit_ax, self.orbit_ay, self.orbit_az = self.get_acceleration(self.orbit.y[:3,:self.orbit_timesteps])
-        self.orbit_rewards = 0
+        # self.phase_coords = self.init_params
+        # self.orbit = self._calculate_orbit()
+        # self.orbit_timesteps = len(self.orbit.y[0])
+        # self.orbit_ax, self.orbit_ay, self.orbit_az = self.get_acceleration(self.orbit.y[:3,:self.orbit_timesteps])
+        # self.orbit_rewards = 0
         return self.init_params, self._get_info()
 
     def render(self, mode='human'):
         return
     
     def _calculate_orbit(self):
-        return solve_ivp(self.get_equations, t_span=(0, self.orbit_duration), y0=self.init_params, t_eval=np.linspace(0, self.orbit_duration, self.orbit_timesteps))
+        return solve_ivp(self.get_equations, t_span=(0, self.orbit_duration), y0=self.init_params, t_eval=np.linspace(0, self.orbit_duration, self.orbit_timesteps)).y[:]
         
     def get_acceleration(self, pos):
         ax, ay, az = 0, 0, 0
@@ -88,11 +95,15 @@ class Dynamics(gym.Env):
         self.galaxy_model_list = ['point_source']
         self.galaxy_model_kwargs_list = [{}]
         self.seed = 0
-        self.device = torch.device('cuda' if torch.cuda.is_available else 'cpu')
+        self.cuda = False
+        self.predicted_orbit_delta_list = [10]
+        self.orbit_model_epochs = int(1e4)
         self.orbit_timesteps = 1000
         self.orbit_duration = 100 # Myr
         for param, val in hyperparameters.items():
             exec('self.' + param + ' = ' + '%s'%val)
+
+        self.device = torch.device('cuda' if self.cuda and torch.cuda.is_available else 'cpu')
 
         if self.seed != None:
             assert(type(self.seed) == int)
@@ -103,14 +114,8 @@ class Dynamics(gym.Env):
     def _get_info(self):
         return {}
     
-    def _get_info_2(self):
-        return {}
-    
     def _process_actions(self, action):
-        return np.clip(action, self.low / 10, self.high[:3] / 10)
-
-    def _process_actions_2(self, action):
-        return np.clip(self._denormalise_state(action), self.low, self.high[:3])
+        return np.clip(action, self.low / 10, self.high / 10)
     
     def _normalise_state(self, state):
         return state / self.high
@@ -118,5 +123,14 @@ class Dynamics(gym.Env):
     def _denormalise_state(self, state):
         return state * self.high
 
-    def chi_sq(self, true_val, pred_val, sigma=1):
-        return np.sum((true_val - pred_val)**2 / sigma**2)
+    def error(self, true_val, pred_val):
+        return torch.sum(((true_val - pred_val)/pred_val)**2)
+    
+    def create_orbit_model(self, hidden_dim=64):
+        return torch.nn.Sequential(
+            torch.nn.Linear(self.observation_space.shape[0], hidden_dim),
+            torch.nn.ReLU(),
+            # torch.nn.Linear(hidden_dim, hidden_dim),
+            # torch.nn.ReLU(),
+            torch.nn.Linear(hidden_dim, self.observation_space.shape[0]),
+        )
