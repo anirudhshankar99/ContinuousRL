fixed units + started rocket

Dynamics/dynamics_env/dynamics_ma.py
@@ -21,6 +21,9 @@ class Dynamics(gym.Env):
         low = np.repeat(np.expand_dims(self.low, 0), self.num_agents, 0).flatten()
         self.high_cat = high
         self.low_cat = low
+        self.km_to_pc = 3.241e-14
+        self.Myr_to_sec = 86400 * 365 * 1e6
+        self.orbit_duration *= self.Myr_to_sec
         self.action_space = gym.spaces.Box(
             low=self.low,
             high=self.high,
@@ -79,16 +82,28 @@ class Dynamics(gym.Env):
         t = np.linspace(t_span[0], t_span[1], n_steps)
         acc = np.array(ode_function(pos[0], t[0]))
         for i in range(1, n_steps):
-            v_half = vel[i-1] + 0.5 * delta_t * acc
-            pos[i] = pos[i-1] + delta_t * v_half
+            v_half = vel[i-1] + 0.5 * delta_t * acc * self.km_to_pc
+            pos[i] = pos[i-1] + delta_t * v_half * self.km_to_pc
+            # origin capture
+            w = pos[i] - pos[i-1]
+            for galaxy_model in self.stationary_potentials:
+                v = galaxy_model.pos - pos[i-1]
+                w_dot_v = self.dot_product(w, v)
+                v_dot_v = self.dot_product(v, v)
+                t_ = w_dot_v / v_dot_v
+                T = pos[i-1] + np.reshape(t_, (-1,1)) * v
+                origin_capture =  np.linalg.norm((T - galaxy_model.pos), axis=-1) < self.origin_capture_delta * self.box_scaling
+                vel[i][origin_capture] = np.zeros_like(vel[i][origin_capture])
+                pos[i][origin_capture] = T[origin_capture]
+                if np.any(origin_capture): break
             new_acc = np.array(ode_function(pos[i], t[i]))
-            origin_capture = new_acc == np.inf
-            origin_capture = np.any(origin_capture, axis=-1)
-            vel[i] = v_half + 0.5 * delta_t * new_acc
-            vel[i][origin_capture] = np.zeros_like(vel[i][origin_capture])
-            pos[i][origin_capture] = pos[i-1][origin_capture]
-            print(pos[i])
+            vel[i][~origin_capture] = (v_half + 0.5 * delta_t * new_acc * self.km_to_pc)[~origin_capture]
             acc = new_acc
+            acc[origin_capture] = np.zeros_like(acc[origin_capture])
+            if np.all(origin_capture):
+                pos[i:] = pos[i]
+                vel[i:] = vel[i]
+                break
         orbit_y = np.reshape(np.concat([pos, vel], axis=-1), (n_steps, -1)).transpose()
         return Orbit(orbit_y, np.linspace(t_span[0], t_span[1], n_steps))
 
@@ -149,10 +164,11 @@ class Dynamics(gym.Env):
         self.dynamic_potential_kwargs_list = [{}]
         self.seed = 0
         self.cuda = False
-        self.orbit_timesteps = 1000
-        self.orbit_duration = 10000 # Myr
+        self.orbit_timesteps = 100
+        self.orbit_duration = 100 # Myr
         self.box_scaling = 1
         self.damping_delta = 0.1
+        self.origin_capture_delta = 1e-1
         for param, val in hyperparameters.items():
             exec('self.' + param + ' = ' + '%s'%val)
 
@@ -187,3 +203,6 @@ class Dynamics(gym.Env):
     
     def out_of_bounds_damping(self, r_max):
         return (1 + self.damping_delta)*np.e**(-1) if r_max < self.high[0] else np.e**(-r_max / self.high[0])+self.damping_delta*np.e**(-1)
+    
+    def dot_product(self, a, b):
+        return np.sum(np.einsum('...i,...i->...i',a,b), axis=-1)

Dynamics/dynamics_env/galaxy_models.py
@@ -1,8 +1,7 @@
 import numpy as np
 
 G_IN_PC_KMS = 4.30091e-3
-origin_capture_acc = 7500
-origin_capture_delta = 1e-4
+MYR_TO_SEC = 86400 * 365 * 1e6
 
 def add_galaxy_model(model_name, **kwargs):
     assert model_name in model_mapping, "Unsupported galaxy model: %s"%model_name
@@ -70,13 +69,13 @@ class PointSource():
         elif len(pos.shape) < len(selfpos.shape):
             pos = np.expand_dims(pos, axis=[i for i in range(len(pos.shape), len(selfpos.shape))])
         del_r = pos - selfpos # in pc
-        if np.linalg.norm(del_r, axis=-1) < origin_capture_delta:
-            return np.split(np.ones_like(del_r) * np.inf, 3, axis=-1)
+        # if np.linalg.norm(del_r, axis=-1) < origin_capture_delta:
+        #     return np.split(np.ones_like(del_r) * np.inf, 3, axis=-1)
         r = np.linalg.norm(del_r, axis=-1) # in pc
-        a = -G_IN_PC_KMS * self.M / (r**3) * del_r
+        a = -G_IN_PC_KMS * self.M / (r**3 + 1e-5) * del_r
         ax, ay, az = np.split(a, 3, axis=-1)
-        if np.linalg.norm(a, axis=-1) > origin_capture_acc:
-            return np.split(np.ones_like(a) * np.inf, 3, axis=-1)
+        # if np.linalg.norm(a, axis=-1) > origin_capture_acc:
+        #     return np.split(np.ones_like(a) * np.inf, 3, axis=-1)
         return ax, ay, az
     
 class Bulge():
@@ -153,7 +152,7 @@ class Bar():
         self.a = a
         self.b = b
         self.c = c
-        self.omega_p = omega_p
+        self.omega_p = omega_p / MYR_TO_SEC
         self.pos = np.array(pos)
         self.sign = 'bar'
 
@@ -163,8 +162,6 @@ class Bar():
         if selfpos is None: selfpos = self.pos
 
         del_r = spatial_pos - selfpos
-        if np.linalg.norm(del_r, axis=-1) < origin_capture_delta:
-            return np.split(np.ones_like(del_r) * np.inf, 3, axis=-1)
         theta = self.omega_p * t
         cos_t, sin_t = np.cos(theta), np.sin(theta)
         rotation_matrix = np.array(np.stack([np.concat([cos_t, -sin_t, np.zeros_like(theta)], axis=-1),
@@ -178,8 +175,6 @@ class Bar():
         rotation_matrix_T = np.transpose(rotation_matrix, axes=(-2,-1))
         A_intertial_frame = np.einsum('...j,ij->...i', A, rotation_matrix_T)
         ax, ay, az = np.split(A_intertial_frame, 3, axis=-1)
-        if np.linalg.norm(A_intertial_frame, axis=-1) > origin_capture_acc:
-            return np.split(np.ones_like(A_intertial_frame) * np.inf, 3, axis=-1)
         return ax, ay, az
 
 model_mapping = {

Dynamics/ppo_continuous_ma.py
@@ -155,10 +155,13 @@ if __name__ == '__main__':
             env = Dynamics(hyperparameters={
                 'stationary_potential_list':['bar'],
                 'stationary_potential_kwargs_list':[{'M':1e10, 'a':5000, 'b':1500, 'c':1000, 'omega_p':0.04}],
-                'dynamic_potential_list':['point_source','point_source'],
+                # 'dynamic_potential_list':['point_source','point_source'],
+                'dynamic_potential_list':['tracer','tracer'],
                 'dynamic_potential_kwargs_list':[{'M':1e4}, {'M':1e4}],
                 'seed':seed,
                 'box_scaling':args.box_scaling,
+                'orbit_duration':100,
+                'orbit_timesteps':100,
             })
             env.action_space.seed(seed)
             env.observation_space.seed(seed)
@@ -271,7 +274,7 @@ if __name__ == '__main__':
             progress.set_description(f'episodic_reward: {episodic_reward}')
             progress.update()
     state_list = np.array(state_list)
-    save_mask = state_list[:,0] > np.max(state_list[:,0]) / 2
+    save_mask = state_list[:,0] > (np.max(state_list[:,0]) / 2 + np.mean(state_list[:,0]))
     columns = ['reward']
     for agent in range(args.num_agents):
         columns += ['x', 'y', 'z', 'vx', 'vy', 'vz']